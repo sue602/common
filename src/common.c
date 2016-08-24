7a95a25665f2ef910f6a7df25f0269a2d348e2cb
suyinxiang <3119017255@qq.com>
2016-08-24 17:57:54 +0800

    test

--- src/common.c
+++ src/common.c
@@ -14,7 +14,7 @@
 //easy_malloc,easy_realloc,size can't be grater than 2^24 - 1
 typedef uint32_t common_uint32;
 #define MAKE_PREFIX_SIZE(t,sz) ( (t<<24) | sz )
-#define PRE_TYPE(makesize) ( makesize>>24 )
+#define PRE_TYPE(makesize) ( (makesize>>24) & 0x000000ff )
 #define PRE_SIZE(makesize) (makesize & 0x00ffffff )
 #define POW2(n) (1<<(n+4)) //begin is BUF16 = 2^4
 
@@ -72,8 +72,8 @@ void common_init()
 	{
 		g_bufs[i].data = listCreate();
 		SPIN_INIT(&g_bufs[i])
-		short j = 0;
-		short weight = MALLOC_NUMBERS / (i+1) ;
+		int j = 0;
+		int weight = MALLOC_NUMBERS / (i+1) ;
 		for(;j<weight;j++)
 		{
 			int sz = POW2(i);
@@ -81,7 +81,9 @@ void common_init()
 			*( (common_uint32*)allocbuf) = MAKE_PREFIX_SIZE(i,sz);
 			listAddNodeTail(g_bufs[i].data, allocbuf );
 		}
+		printf("buf[%d],length=%d weight=%d\n",i,listLength(g_bufs[i].data),weight);
 	}
+	printf("common init ok\n");
 }
 
 void common_fini()
@@ -130,6 +132,7 @@ void * easy_malloc(uint32_t sz)
 	void * buf = NULL;
 	if(0 == listLength(g_bufs[buft].data))
 	{
+		printf("not enought ======sz =%d  type=%d\n",sz,buft);
 		unsigned int malloc_sz = POW2(buft);
 		void * allocbuf = zmalloc(malloc_sz);//2^N
 		listAddNodeTail(g_bufs[buft].data,allocbuf);
@@ -145,7 +148,7 @@ void * easy_malloc(uint32_t sz)
 	}
 	listDelNode(g_bufs[buft].data,node);
 	SPIN_UNLOCK(&g_bufs[buft])
-	printf("easy malloc %p ,type=%d ,sz=%d make int=%d len=%d\n",buf,buft,sz,MAKE_PREFIX_SIZE(buft,sz),len);
+	// printf("easy malloc %p ,type=%d ,sz=%d make int=%d len=%d\n",buf,buft,sz,MAKE_PREFIX_SIZE(buft,sz),len);
 	return buf;
 }
 
@@ -161,10 +164,10 @@ void easy_free(void * p)
 		exit(0);
 	}
 	enum buf_type buft = type ;
-
+	printf("easy_free ==========p=%p,sz =%d  type=%d\n",p,oldsize,buft);
 	SPIN_LOCK(&(g_bufs[buft]))
 
-	printf("easy free %p ,size=%d,type=%d pre_data=%d\n",p,oldsize,type,pre_data);
+	// printf("easy free %p ,size=%d,type=%d pre_data=%d len=%d\n",p,oldsize,type,pre_data,len);
 	//printf("before free=%d\n",listLength(g_bufs[buft].data));
 
 //	listAddNodeHead(g_bufs[buft].data,p);
@@ -177,14 +180,17 @@ void easy_free(void * p)
 
 void * easy_realloc(void * p,uint32_t sz)
 {
-	if (NULL == p)
+	if (NULL == p && sz > 0)
+	{
 		return easy_malloc(sz);
+	}
 
 	if(0 == sz)
 	{
 		easy_free(p);
 		return NULL;
 	}
+
 	size_t len = zmalloc_size(p);
 	common_uint32 pre_data = * ( (common_uint32*)(p+len-COMMON_PRE_SIZE) );
 	short type = PRE_TYPE(pre_data);
@@ -197,13 +203,22 @@ void * easy_realloc(void * p,uint32_t sz)
 	enum buf_type buft = type ;
 
 	unsigned int malloc_sz = POW2(buft);
-	if(sz > oldsize && sz > malloc_sz )
+
+	enum buf_type newbuft = _check_sz(sz+COMMON_PRE_SIZE);//预先找最合适大小
+
+	if(buft < newbuft )
 	{
+		printf("bigger=================p=%p,oldsize=%d,sz=%d,oldsize_pow2=%d\n type=%d ,pre_data=%d,len=%d\n",p,oldsize,sz,malloc_sz,type,pre_data,len );
 		void * oldp = p;
 		void * allocbuf = easy_malloc(sz);
 		memcpy(allocbuf,p,oldsize);//copy data
 		easy_free(oldp);//free p
+		// printf("easy free ok=\n");
 		p = allocbuf;
+	}
+	else
+	{
+		*( (common_uint32*) (p+len-COMMON_PRE_SIZE) ) = MAKE_PREFIX_SIZE(buft,sz);
 	}
 	return p;
 }
